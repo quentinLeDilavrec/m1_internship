5db9abf28980cc4964b98b46a5dc9fc839df9402
quentin <ledilavrecquentin@gmail.com>
2019-06-09 10:24:59 +0200

    fix eol

---
+++ start-instrumented-chrome/something.ts
@@ -0,0 +1,33 @@
+ // TODO find why this line avoid Snap
+ // blzbl
+///
+
+function aaabbbccc(params) {
+	params;
+	console.log(9684);
+	return 768111;
+}
+function aaababbccc(params) {
+	params;
+	console.log(9684);
+	return 768111;
+}
+let a = () => {
+	return 5;
+};
+class namez {
+	constructor(parameters) {
+		
+	}
+	
+	public get value() : string {
+		return 
+	}
+	
+}
+
+let x = {
+	f(){
+		
+	}
+}
